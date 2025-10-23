8ca7a9aa7bd39ca86cfb1a1c1cfb9b7192056ff1
tanmaypaliwal576 <tanmayprofessional576@gmail.com>
2025-10-23 18:19:35 +0530

    signup controller added

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectdb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected", conn.connection.host);
+  } catch (error) {
+    console.log("error", error);
+    process.exit(1);
+  }
+};
